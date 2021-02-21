e5369aad2bbd8c9c6847696a3cf73c3083d543da
zhongsy <2388291240@qq.com>
2021-02-21 14:42:08 +0800

    数据库配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'itheima',
+        'USER': 'root',
+        'PASSWORD': '252525',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
